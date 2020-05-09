bd5bce0ce05e4a1751b47686d67105f9101e813f
Lucas Oliveira <48152939+OliveiraLucas10@users.noreply.github.com>
2020-05-09 07:02:41 +0100

    Add files via upload

---
+++ src/main/java/com/oliveiralucaspro/olpjms/OlpJmsApplication.java
@@ -0,0 +1,13 @@
+package com.oliveiralucaspro.olpjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlpJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlpJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oliveiralucaspro/olpjms/OlpJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveiralucaspro.olpjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlpJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
